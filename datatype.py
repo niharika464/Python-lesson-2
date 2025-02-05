a174ad1701bc28c33676f086c6a61a729fe73019
niharika464 <niharikasinghgaur11@gmail.com>
2025-02-05 21:56:23 +0530

    data types

---
+++ datatype.py
@@ -0,0 +1,16 @@
+name= "Niharika"
+age=18  #float
+height=153.4  #integer
+status=True  #boolean
+
+print(age)
+print("my height is:",height)
+print(status)
+print(name)
+
+age=float(18)
+print(age)
+height=int(153.4)
+print(height)
+status=True
+print(status)
---
+++ pyop.py
@@ -0,0 +1,16 @@
+n1=70
+n2=35
+
+print("Add-",n1+n2)
+print("sub-",n1-n2)
+print("mul-",n1*n2)
+print("div-",n1/n2)
+print("flordiv-",n1//n2)
+print("mod-",n1%n2)
+print("exp-",n1**n2)
+
+print(n1==n2)
+print(n1!=n2)
+print(n1>=n2)
+print(n1<=n2)
+print(n1=n2)
