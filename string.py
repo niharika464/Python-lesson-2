fee5b8e848070e4a50b1fb46c519665589fea5aa
niharika464 <niharikasinghgaur11@gmail.com>
2025-02-05 22:20:03 +0530

    python operator

---
+++ string.py
@@ -0,0 +1,8 @@
+fn="Niharika"
+ln="Gaur"
+
+print("fullname:",fn+ln)
+new="Niharika"*4
+print(new)
+print(fn[0:5])
+print(fn[-1])
---
+++ swap.py
@@ -0,0 +1,12 @@
+name=input("enter your name:")
+a=int(input("enter value a:"))
+b=float(input("enter value b:"))
+
+
+empty=a
+a=b
+b=empty
+
+print("value of a: ", a)
+
+print("value of b: ", b)
